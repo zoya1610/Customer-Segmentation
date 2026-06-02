58985d14163d8d94174dc0b92a096b0067c709f2
Zoya Mirza <zoya16.info@gmail.com>
2026-06-02 20:50:44 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.write("Enter Customer Details to predict the segment.")
 
 age=st.number_input("Age", min_value=18,max_value = 100, value = 35)
 income=st.number_input("Income", min_value=0,max_value = 200000, value = 50000)
-total_spending==st.number_input("Total_Spending (Sum of Purchase)", min_value=0 ,max_value = 5000, value = 1000)
+total_spending=st.number_input("Total_Spending (Sum of Purchase)", min_value=0 ,max_value = 5000, value = 1000)
 num_web_purchases=st.number_input("Number of Web Purchases", min_value = 0, max_value= 100, value=10) 
 num_store_purchases=st.number_input("Number of Store Purchases", min_value = 0, max_value= 100, value=10) 
 num_web_visits=st.number_input("Number of Web Visits for Months", min_value = 0, max_value= 50, value=4) 
